53d1fc100dda4f2e8c2df718763836461f18f134
carmenpercar <43983865+carmenpercar@users.noreply.github.com>
2021-10-29 10:48:37 +0200

    Spawn Manager and Ball prefab

---
+++ Assets/Resources/Scripts/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private float secondsToDestroy = 10f;
+    private float elapsedSecondsToDestroy;
+
+    private void Update() {
+        elapsedSecondsToDestroy += Time.deltaTime;
+        if(elapsedSecondsToDestroy >= secondsToDestroy) {
+            DestroyBall();
+        }
+    }
+    private void DestroyBall() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Resources/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+    [SerializeField] private float forceValue;
+    [SerializeField] private float rotation;
+
+    private GameObject spawnedBall;
+
+    private void Update() {
+        RotateSpawner();
+        if(Input.GetButtonDown("Fire1")) {
+            SpawnBall();
+        }
+    }
+    private void SpawnBall() {
+        spawnedBall = Instantiate(ball, transform.position, transform.rotation);
+        spawnedBall.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+        spawnedBall.GetComponent<Rigidbody>().AddForce(transform.forward * forceValue, ForceMode.Impulse);
+    }
+    private void RotateSpawner() {
+        transform.Rotate(0, rotation, 0);
+    }
+}
